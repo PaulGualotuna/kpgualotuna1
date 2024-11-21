c9c7e2822b08dafd326b8cdb1f99e726993ab07f
kpgualotuna1 <160292023+PaulGualotuna@users.noreply.github.com>
2024-11-21 16:33:03 -0500

    Delete 3788_kpgualotuna1_codigo/js/login.js

--- 3788_kpgualotuna1_codigo/js/login.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('btn-login').addEventListener('click', function (event) {
-    event.preventDefault(); // Prevenir comportamiento predeterminado
-    window.location.href = 'formularioLogin.html'; // Redirigir al formulario de login
-});
